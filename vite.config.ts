0e0fea93b76baf09f8dc03a2fbdf055c0680d9e2
FabrizioSulcaR <fabriziosulca28@gmail.com>
2025-04-13 17:57:15 -0500

    .,

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
